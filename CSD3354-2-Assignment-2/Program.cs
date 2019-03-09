85f3feab96eac1768ea262d36a8b31de52f37357
hps3103 <ssingh.harpushpam@gmail.com>
2019-03-08 21:25:02 -0500

    Lab16

--- CSD3354-2-Assignment-2/Program.cs
+++ CSD3354-2-Assignment-2/Program.cs
@@ -2,36 +2,34 @@
 
 namespace DelegatesAndEvents
 {
-    public class program
+    public class Program
     {
         public static void Main()
         {
-            {
-                DelegateExercises delegateExercises = new DelegateExercises();
-                delegateExercises.Method3();
-                Console.ReadLine();
-            }
+            DelegateExercises delegateExercises = new DelegateExercises();
+            delegateExercises.Method3();
+            Console.ReadLine();
+
         }
-        public delegate int MyDelegate(out int i);
-        public class DelegateExercises
+    }
+
+    public delegate void MyDelegate(ref int intValue);
+
+    public class DelegateExercises
+    {
+        void Method1(ref int intValue)
         {
-            int Method1(out int i)
-            {
+            intValue = intValue + 5;
+            System.Console.WriteLine("Method1 " + intValue);
+        }
 
-                i = 100;
-                System.Console.WriteLine("Method1" + i);
-                return 0;
-            }
-            public void Method3()
-            {
-                MyDelegate myDelegate = new MyDelegate(Method1);
-                MyDelegate myDelegate1 = null;
-                MyDelegate myDelegate2 = myDelegate + myDelegate1;
-                int intValue;
-                myDelegate2(out intValue);
-            }
-
-
+        public void Method3()
+        {
+            MyDelegate myDelegate = new MyDelegate(Method1);
+            MyDelegate myDelegate1 = new MyDelegate(Method1);
+            MyDelegate myDelegate2 = myDelegate + myDelegate1;
+            int intParameter = 5;
+            myDelegate2(ref intParameter);
         }
     }
 
