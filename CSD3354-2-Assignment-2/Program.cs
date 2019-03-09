94603d820198dc61a5bfb16a5c1ad10637f1b6b2
hps3103 <ssingh.harpushpam@gmail.com>
2019-03-08 20:09:45 -0500

    Lab05

--- CSD3354-2-Assignment-2/Program.cs
+++ CSD3354-2-Assignment-2/Program.cs
@@ -6,34 +6,28 @@ using System.Threading.Tasks;
 
 namespace DelegatesAndEvents
 {
+    public class Program
+    {
+        public static void Main()
+        {
+            DelegateExercises delegateExercises = new DelegateExercises();
+            delegateExercises.Method3();
+        }
+    }
+
     public class DelegateExercises
     {
-        public delegate int MyDelegate(int intValue);
-
-        public static void Main(string[] args)
+        public delegate int MyDelegate();      
+         public int Method1()
         {
-            DelegateExercises de = new DelegateExercises();
-            de.Method3();
-        }
-
-        public int Method1(int intMethod1)
-        {
-            return intMethod1 * 2;
-            
-        }
-        public int Method2(int intMethod2)
-        {
-            return intMethod2 * 10;
+            Console.WriteLine("MyDelegate");
+            return 1;
         }
 
         public void Method3()
         {
-            MyDelegate myDelegate = new MyDelegate(Method1);
-            int result1 = myDelegate(10);
-            Console.WriteLine(result1);
-            myDelegate = new MyDelegate(Method2);
-            int result2 = myDelegate(10);
-            Console.WriteLine(result2);
+            MyDelegate myDelegate = new MyDelegate(Method1);           
+            myDelegate();         
             Console.ReadLine();
         }
     }
