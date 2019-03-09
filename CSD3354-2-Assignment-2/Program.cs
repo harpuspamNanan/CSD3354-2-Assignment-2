aba31ef7913f0f109e79308733048d2307143c88
hps3103 <ssingh.harpushpam@gmail.com>
2019-03-08 20:39:56 -0500

    Lab06

--- CSD3354-2-Assignment-2/Program.cs
+++ CSD3354-2-Assignment-2/Program.cs
@@ -1,8 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace DelegatesAndEvents
 {
@@ -12,23 +8,37 @@ namespace DelegatesAndEvents
         {
             DelegateExercises delegateExercises = new DelegateExercises();
             delegateExercises.Method3();
+            Console.ReadLine();
         }
     }
 
     public class DelegateExercises
     {
-        public delegate int MyDelegate();      
-         public int Method1()
+        public delegate int MyDelegate(int intValue);
+
+        public int Method1(int intMethod1)
         {
-            Console.WriteLine("MyDelegate");
-            return 1;
+            return intMethod1 * 2;
+        }
+
+        public int Method2(int intMethod1)
+        {
+            return intMethod1 * 10;
+        }
+
+        public void Method4(MyDelegate myDelegate)
+        {
+            int result = myDelegate(10);
+            Console.WriteLine(result);
         }
 
         public void Method3()
         {
-            MyDelegate myDelegate = new MyDelegate(Method1);           
-            myDelegate();         
-            Console.ReadLine();
+            MyDelegate myDelegate = new MyDelegate(Method1);
+            Method4(myDelegate);
+            myDelegate = new MyDelegate(Method2);
+            Method4(myDelegate);
+            
         }
     }
    
