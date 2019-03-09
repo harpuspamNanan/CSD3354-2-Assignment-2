79066ad0d17ce041313dec4de3f06d6ae3060e6a
hps3103 <ssingh.harpushpam@gmail.com>
2019-03-08 19:54:12 -0500

    Lab04

--- CSD3354-2-Assignment-2/Program.cs
+++ CSD3354-2-Assignment-2/Program.cs
@@ -8,25 +8,34 @@ namespace DelegatesAndEvents
 {
     public class DelegateExercises
     {
-        public delegate void MyDelegate(int num);
+        public delegate int MyDelegate(int intValue);
 
         public static void Main(string[] args)
         {
             DelegateExercises de = new DelegateExercises();
-            de.Method2();
+            de.Method3();
         }
 
-        void Method1(int i)
+        public int Method1(int intMethod1)
         {
-            Console.WriteLine("Method1");
-            Console.ReadLine();
+            return intMethod1 * 2;
+            
         }
-        public void Method2()
+        public int Method2(int intMethod2)
+        {
+            return intMethod2 * 10;
+        }
+
+        public void Method3()
         {
             MyDelegate myDelegate = new MyDelegate(Method1);
-            myDelegate(60);
+            int result1 = myDelegate(10);
+            Console.WriteLine(result1);
+            myDelegate = new MyDelegate(Method2);
+            int result2 = myDelegate(10);
+            Console.WriteLine(result2);
+            Console.ReadLine();
         }
     }
-
    
 }
