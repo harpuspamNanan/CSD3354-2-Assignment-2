47b6f770b1c0346b6266ac9a9926d2a91ce84bcc
hps3103 <ssingh.harpushpam@gmail.com>
2019-03-08 21:23:14 -0500

    Lab14 Updated

--- CSD3354-2-Assignment-2/Program.cs
+++ CSD3354-2-Assignment-2/Program.cs
@@ -2,38 +2,36 @@
 
 namespace DelegatesAndEvents
 {
-    public class Program
+    public class program
     {
         public static void Main()
         {
-            DelegateExercises delegateExercises = new DelegateExercises();
-            try
             {
+                DelegateExercises delegateExercises = new DelegateExercises();
                 delegateExercises.Method3();
                 Console.ReadLine();
             }
-            catch (System.Exception ex)
+        }
+        public delegate int MyDelegate(out int i);
+        public class DelegateExercises
+        {
+            int Method1(out int i)
             {
-                System.Console.WriteLine("Exception Occurred.");
-                Console.ReadLine();
 
+                i = 100;
+                System.Console.WriteLine("Method1" + i);
+                return 0;
             }
-        }
-    }
+            public void Method3()
+            {
+                MyDelegate myDelegate = new MyDelegate(Method1);
+                MyDelegate myDelegate1 = null;
+                MyDelegate myDelegate2 = myDelegate + myDelegate1;
+                int intValue;
+                myDelegate2(out intValue);
+            }
 
-    public delegate void MyDelegate();
 
-    public class DelegateExercises
-    {
-        void Method1()
-        {
-            throw new System.Exception();
-        }
-
-        public void Method3()
-        {
-            MyDelegate myDelegate = new MyDelegate(Method1);
-            myDelegate();
         }
     }
 
